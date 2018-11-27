dcb01aeafdef8e0c5d2a9063966cf9880b50d771
Pedro <pedro.alvarez412@outlook.com>
2018-11-26 20:27:21 -0600

    End of Section 3

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers {
 
-    // http://localhost:5000/api/values
+    [Authorize]
     [Route ("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase {
@@ -16,7 +17,7 @@ namespace DatingApp.API.Controllers {
         public ValuesController (DataContext context) {
             _context = context;
         }
-        // GET api/values
+
         [HttpGet]
         public async Task<IActionResult> GetValues () {
             var values = await _context.Values.ToListAsync ();
@@ -24,7 +25,7 @@ namespace DatingApp.API.Controllers {
             return Ok (values);
         }
 
-        // GET api/values/5
+        [AllowAnonymous]
         [HttpGet ("{id}")]
         public async Task<IActionResult> GetValue (int id) {
             var value = await _context.Values.FirstOrDefaultAsync (x => x.Id == id);
